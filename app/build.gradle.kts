a80430ef065ead2f9b05c94bc7ea3626b49db413
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-12-03 01:44:30 +0530

    Retrofit setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,9 @@ dependencies {
 
     val nav_version = "2.7.5"
     implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
