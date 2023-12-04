b79390a533c20c182be81b4503b098bebda990d3
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-12-04 17:42:17 +0530

    Add network connectivity observer

---
+++ app/src/main/java/me/ashutoshkk/blogapp/common/ConnectivityObserver.kt
@@ -0,0 +1,11 @@
+package me.ashutoshkk.blogapp.common
+
+import kotlinx.coroutines.flow.Flow
+
+interface ConnectivityObserver {
+    fun observe(): Flow<Status>
+
+    enum class Status {
+        Available, Unavailable, Losing, Lost
+    }
+}
