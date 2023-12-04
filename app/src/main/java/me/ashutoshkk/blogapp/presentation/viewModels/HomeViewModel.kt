7723210033055361e7ee9080c4a10e702e55f028
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-12-04 17:28:24 +0530

    Fix crash by using cachedIn

--- app/src/main/java/me/ashutoshkk/blogapp/presentation/viewModels/HomeViewModel.kt
+++ app/src/main/java/me/ashutoshkk/blogapp/presentation/viewModels/HomeViewModel.kt
@@ -3,13 +3,11 @@ package me.ashutoshkk.blogapp.presentation.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
-import me.ashutoshkk.blogapp.common.Resource
 import me.ashutoshkk.blogapp.domain.model.Blog
 import me.ashutoshkk.blogapp.domain.useCase.GetBlogsUseCase
 import javax.inject.Inject
@@ -17,7 +15,7 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getBlogsUseCase: GetBlogsUseCase
-): ViewModel() {
+) : ViewModel() {
 
     private val _blogs = MutableStateFlow<PagingData<Blog>>(PagingData.empty())
     val blogs = _blogs.asStateFlow()
@@ -28,9 +26,11 @@ class HomeViewModel @Inject constructor(
 
     private fun getBlogs() {
         viewModelScope.launch {
-            getBlogsUseCase().collect{
-                _blogs.value = it
-            }
+            getBlogsUseCase()
+                .cachedIn(viewModelScope)
+                .collect {
+                    _blogs.value = it
+                }
         }
     }
 }
