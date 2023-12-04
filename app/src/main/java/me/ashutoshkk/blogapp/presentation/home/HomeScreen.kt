3c2aa78d81b5d78689b58dc6c527404edab921c6
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-12-04 09:54:22 +0530

    Add progress bar

--- app/src/main/java/me/ashutoshkk/blogapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/me/ashutoshkk/blogapp/presentation/home/HomeScreen.kt
@@ -2,15 +2,19 @@ package me.ashutoshkk.blogapp.presentation.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import me.ashutoshkk.blogapp.presentation.home.components.BlogItem
 import me.ashutoshkk.blogapp.presentation.viewModels.HomeViewModel
@@ -36,6 +40,46 @@ fun HomeScreen(
             ) { url ->
                 onItemClick(url)
             }
+        }
+        when (blogs.loadState.refresh) {
+            is LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier.fillParentMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+
+            is LoadState.Error -> {
+
+            }
+
+            else -> {}
+        }
+
+        when (blogs.loadState.append) {
+            is LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+
+            is LoadState.Error -> {
+
+            }
+
+            else -> {}
         }
     }
 }
