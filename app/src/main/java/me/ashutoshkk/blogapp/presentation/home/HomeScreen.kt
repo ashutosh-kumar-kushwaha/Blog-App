cfc1e0bad5a1d6a471b13d18bc2bf9e8324f8d3c
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-12-07 12:17:48 +0530

    Observe ConnectivityObserver in HomeScreen

--- app/src/main/java/me/ashutoshkk/blogapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/me/ashutoshkk/blogapp/presentation/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package me.ashutoshkk.blogapp.presentation.home
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,24 +11,30 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import me.ashutoshkk.blogapp.common.ConnectivityObserver
+import me.ashutoshkk.blogapp.common.NetworkConnectivityObserver
 import me.ashutoshkk.blogapp.domain.model.Blog
 import me.ashutoshkk.blogapp.presentation.home.components.BlogItem
 import me.ashutoshkk.blogapp.presentation.viewModels.HomeViewModel
@@ -42,6 +49,14 @@ fun HomeScreen(
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val connectivityObserver =
+        NetworkConnectivityObserver(LocalContext.current)
+    var showConnectedSnackbar = remember {
+        false
+    }
+
+    val connectivityStatus = connectivityObserver.observe()
+        .collectAsStateWithLifecycle(ConnectivityObserver.Status.Unavailable)
 
     Scaffold(
         topBar = {
@@ -72,6 +87,45 @@ fun HomeScreen(
         }
     }
 
+    LaunchedEffect(connectivityStatus.value) {
+        when (connectivityStatus.value) {
+            ConnectivityObserver.Status.Available -> {
+                if(showConnectedSnackbar){
+                    snackbarHostState.showSnackbar(
+                        message = "Internet Connected",
+                        duration = SnackbarDuration.Short
+                    )
+                }
+            }
+
+            ConnectivityObserver.Status.Losing -> {
+                showConnectedSnackbar = true
+
+                snackbarHostState.showSnackbar(
+                    message = "Internet Losing",
+                    duration = SnackbarDuration.Short
+                )
+            }
+
+            ConnectivityObserver.Status.Lost -> {
+                showConnectedSnackbar = true
+
+                snackbarHostState.showSnackbar(
+                    message = "Internet Lost",
+                    duration = SnackbarDuration.Indefinite
+                )
+            }
+
+            ConnectivityObserver.Status.Unavailable -> {
+                showConnectedSnackbar = true
+
+                snackbarHostState.showSnackbar(
+                    message = "Internet Unavailable",
+                    duration = SnackbarDuration.Indefinite
+                )
+            }
+        }
+    }
 }
 
 @Composable
